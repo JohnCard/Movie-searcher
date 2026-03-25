0ae114c81280c9481cc342b88b6255dd35a11d0c
JohnCard <johncard962@gmail.com>
2026-03-24 23:07:39 -0600

    first commit

---
+++ src/MovieSearch.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react"
+
+const apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYTA3ZGFhNjJlNzEzZGEyMWYxMjhkMGJjN2EzZDgzOCIsIm5iZiI6MTcyMTgzNzkwNC41OTUsInN1YiI6IjY2YTEyOTUwMTEyOTU2YTY4MTcyYjJkNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sJIs3FY4JSg8Hh0FrFbER5QDHKpKDhj6m5-X5uxlaaI'
+const urlBase = `https://api.themoviedb.org/3/search/movie?query=`
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${apiKey}`
+    }
+}
+
+export const MovieSearch = () => {
+    const [search, setSearch] = useState('')
+    const [movies, setMovies] = useState([])
+
+    const handleInputChange = (e) => {
+        setSearch(e.target.value)
+    }
+
+    const fetchMovies = async () => {
+        try{
+            const response = await fetch(`${urlBase}${search}&api_key=${apiKey}`, options)
+            const data = await response.json()
+            setMovies(data.results)
+        }catch(error){
+            console.log(`Something went wrong - ${error}`)
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        fetchMovies()
+    }
+  return (
+    <>
+        <div className="container">
+            <h1 className="title">Movie searcher</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Type a movie"
+                    value={search}
+                    onChange={handleInputChange}
+                />
+                <button type="submit" className="search-button">Search</button>
+            </form>
+
+            <div className="movie-list">
+                {movies.map((movie) => (
+                    <div key={movie.id} className="movie-card">
+                        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                        <h2>{movie.title}</h2>
+                    </div>
+                ))}
+            </div>
+        </div>
+    </>
+  )
+}
